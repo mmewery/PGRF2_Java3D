d19edf3bf97758733677b4e28aa506f5f09e6dcd
mmewery <mmewery@gmail.com>
2026-02-16 20:00:44 +0100

    zbuffer v1

--- src/raster/ZBuffer.java
+++ src/raster/ZBuffer.java
@@ -16,7 +16,7 @@ public class ZBuffer {
     public void setPixelWithZTest(int x, int y, double z, Col col) {
         if(depthBuffer.getValue(x, y).isPresent() && z <= depthBuffer.getValue(x, y).get()){
             imageBuffer.setValue(x, y, col);
-            depthBuffer.setValue(x, y, z);
+            depthBuffer.setValue(x, y, Double.valueOf(z));
         }
     }
 }
